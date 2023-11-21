e6240cfb84f56d4cfad4d14a90c8de20ddfdcd04
ElMaikina <mikesoto890@gmail.com>
2023-11-21 17:37:42 -0300

    added gtk and icons theme

--- .bashrc
+++ .bashrc
@@ -2,7 +2,7 @@
 # ~/.bashrc
 #
 
-# install requirements: sudo pacman -Syu exa feh bat grep mupdf mpv ripgrep
+# install requirements: sudo pacman -Syu exa feh bat grep mupdf mpv ripgrep gh taskranger
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
--- prefs.js
+++ prefs.js
@@ -1,14 +1,12 @@
 // Force-enable Hardware Acceleration
 user_pref("gfx.webrender.all", true);
 user_pref("gfx.x11-egl.force-enabled", true);
-
 user_pref("media.rdd-ffmpeg.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
 user_pref("media.av1.enabled", false);
-
 user_pref("widget.dmabuf.force-enabled", true);
 user_pref("media.hardware-video-decoding.force-enabled", true);
-
 // Customization options
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.compactmode.show", true);
